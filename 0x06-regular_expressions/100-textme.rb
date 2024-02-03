4eb23a8bd4237f0c068dae7e170765953e2dd828
sihamfich <sihamfichouche@gmail.com>
2024-02-02 17:05:30 -0800

    Regular expresion Advanced task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,12 +1,9 @@
 #!/usr/bin/env ruby
 
-# Get input from command line argument
-input_string = ARGV[0]
-
 # Extract sender, receiver, and flags from the input string
-sender = input_string[/from:\+*\w*/][5..-1]
-receiver = input_string[/to:\+*\w*/][3..-1]
-flags = input_string[/flags:(.*?)\]/, 1]
+sender = ARGV[0][/from:\+*\w*/][5..-1]
+receiver = ARGV[0][/to:\+*\w*/][3..-1]
+flags = ARGV[0][/flags:(.*?)\]/, 1]
 
 # Format and print the message
 message = "#{sender},#{receiver},#{flags}"
