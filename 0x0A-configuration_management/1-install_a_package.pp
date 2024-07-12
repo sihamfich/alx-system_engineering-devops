c8dbc239509a1846f8f98e19db5e2262a700ac70
SIHAM <sihamfichouche@gmail.com>
2024-07-12 16:00:46 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
-# install flask from pip3 version 2.1.0
-package {'flask':
-	ensure    => '2.1.0',
-	provider  => 'pip'
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
 }
 
+# Install Flask using pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
